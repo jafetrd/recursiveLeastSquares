2c2072ebaf0741bb0ecdaa97fbfac4599e9176ff
Jafet Ríos Durán <jafetrd@gmail.com>
2024-06-13 16:31:34 -0600

    Update main.cpp
    Ejemplo de mínimos cuadrados recursivos donde se guarda cada actualización de la estimación de parámetros

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "fractionalOrderFilter.h"
 #include "estimationFilters.h"
 #include <fstream>
 #include <sstream>
@@ -35,13 +34,14 @@ void _abrirDatos(const std::string& filename, double* arreglo, int L){
 
 int main() {   
     int L = 500; // Cantidad de medicion a usar
-    _abrirDatos("dataInput.txt", u, L); // Archivo con mediciones de entrada
-    _abrirDatos("dataOutput.txt", y, L); // Archivo con mediciones de salida
-    std::ofstream outFile("datos.txt"); // Crear y abrir un archivo de texto
     
     double u[L];
     double y[L];
-        
+    
+    _abrirDatos("dataInput.txt", u, L); // Archivo con mediciones de entrada
+    _abrirDatos("dataOutput.txt", y, L); // Archivo con mediciones de salida
+    std::ofstream outFile("estimatedParams.txt"); // Crear y abrir un archivo de texto
+    
     int M = 3; // Cantidad de coeficientes de numerador a estimar
     int N = 2; // Cantidad de coeficientes de denominador a estimar
     double lambda = 1.0; // Factor de olvido
@@ -54,7 +54,7 @@ int main() {
     for(int i = 0; i < L;i++){
         RLS._estimationRLS(u[i],y[i],theta_hat); 
 
-      //Guardar cada estimacion para verla en Mathematica
+      //Guardar cada estimacion en estimatedParams.txt para verla en Mathematica
         for (int i = 0; i < M+N-1; i++) {
             outFile << std::fixed << std::setprecision(15) << theta_hat[i] << ",";
         }
